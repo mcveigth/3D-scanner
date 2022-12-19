d375ab41b81b8b600dd10cd5b80fa3f7f1d35846
mcveigth <mcveigth@protonmail.com>
2022-12-19 00:49:53 +0000

    modified:   README.md 	new file:   install.sh 	modified:   server/camera.py 	.gitignore

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 3DShock
+## 3DShock is a 3D body scanner which utilizes multiple raspberry pi Boards to take a 360 picture of yourself
+ In order to install it please run the script provided
+`sudo install.sh` script is not working right now so figured yourself how to run it
--- server/camera.py
+++ server/camera.py
@@ -3,17 +3,20 @@
 import settings
 import paramiko
 
-command = "ls -l"
-
-def sshCamera(host:str, username:str, password:str):
+def sshCamera(host:str, username:str, password:str, command="ls"):
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, username=username, password=password)
-    _stdin, _stdout,_stderr = client.exec_command("hostname -I")
+    _stdin, _stdout,_stderr = client.exec_command("hostname -I") #should be modify to use mount -o remount, rm
     print(_stdout.read().decode())
     client.close()
 
+def sshAllCameras(cameras):
+    for cam in cameras:
+        print(cam)
+
 if __name__ == '__main__':
-    for ip in settings.RASPBERRY_IPS:
-        if(ip != "192.168.99.106"):
-            sshCamera(ip, settings.RASPBERRY_USER, settings.RASPBERRY_PASS)
+    #for ip in settings.RASPBERRY_IPS:
+    #    if(ip != "192.168.99.106"):
+    #        sshCamera(ip, settings.RASPBERRY_USER, settings.RASPBERRY_PASS)
+    sshAllCameras(["values", "more values", "even more values"])
