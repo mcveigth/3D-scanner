e9c97697ec4d9b3dbeeca1d8c86da5ecfb170a64
mcveigth <mcveigth@protonmail.com>
2023-01-08 16:10:37 -0700

    camera script

--- server/camera.py
+++ server/camera.py
@@ -7,16 +7,17 @@ def sshCamera(host:str, username:str, password:str, command="ls"):
     client = paramiko.client.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, username=username, password=password)
-    _stdin, _stdout,_stderr = client.exec_command("hostname -I") #should be modify to use mount -o remount, rm
+    _stdin, _stdout,_stderr = client.exec_command("mount -o remount,rw / && cd /camsoft/ && pwd && rm options.cfg && echo -ISO 100 -ss 5000 -awb off -awbg 1.304,1.667 -mm matrix -drc off -h 972 -w 1296 -q 100 -t 1500 -br -bs '-65995' -bm -ev 0 -ex off -co 0 -sa 0 >> options.cfg") #should be modify to use mount -o remount, rm
+    #_stdin, _stdout,_stderr = client.exec_command("cd /camsoft/")
+    #_stdin, _stdout,_stderr = client.exec_command("pwd")
+    
+    #_stdin, _stdout,_stderr = client.exec_command("rm options.cfg")
+    #_stdin, _stdout,_stderr = client.exec_command("echo -ISO 100 -ss 5000 -awb off -awbg 1.304,1.667 -mm matrix -drc off -h 972 -w 1296 -q 100 -t 1500 -br -bs '-65995' -bm -ev 0 -ex off -co 0 -sa 0 >> options.cfg")
+    #_stdin, _stdout,_stderr = client.exec_command("pwd")
     print(_stdout.read().decode())
     client.close()
 
-def sshAllCameras(cameras):
-    for cam in cameras:
-        print(cam)
-
 if __name__ == '__main__':
-    #for ip in settings.RASPBERRY_IPS:
-    #    if(ip != "192.168.99.106"):
-    #        sshCamera(ip, settings.RASPBERRY_USER, settings.RASPBERRY_PASS)
-    sshAllCameras(["values", "more values", "even more values"])
+    for ip in settings.RASPBERRY_IPS:
+        sshCamera(ip, settings.RASPBERRY_USER, settings.RASPBERRY_PASS)
+        print("done ip: " + ip)
