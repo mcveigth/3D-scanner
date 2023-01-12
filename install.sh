d9a34ff6738509b153b16a92876bc3c662ba4b4d
Mcveigth <102336370+mcveigth@users.noreply.github.com>
2023-01-12 07:33:55 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #~/bin/bash
 sudo apt update
-
+# install requirements
+sudo apt install -y python3 python3-pip python3-venv
+#create virtual environment
+python3 -m venv env 
+#export path
+echo "export PATH="~/.local/bin:$PATH"" >> ~/.bashrc
+# Activate the environment and install requirements.txt 
+source env/bin/activate 
+python3 -m pip install -r requirements.txt 
